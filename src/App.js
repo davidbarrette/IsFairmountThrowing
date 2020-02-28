bde5d9f1095ffb554cbff94580381233696d34ea
colinkirby <ckirby1@macalester.edu>
2020-02-28 13:34:16 -0600

    Updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import frog1 from './assets/frog1.png'
+import frog2 from './assets/frog2.png'
+import frog3 from './assets/frog3.png'
+
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       throwing: false,
-      date: "probably in a few weeks"
+      date: "Not until Franklin & Zoraiz pass their econ exams"
     }
   }
 
@@ -17,7 +22,10 @@ class App extends React.Component {
       return (
         <div className = "App-throwing">
           <p style={{fontSize: '80px', margin: 0}}> YES </p>
-          <p style={{fontSize: '15px'}}> {this.state.date} </p>
+          <p style={{fontSize: '25px'}}> {this.state.date} </p>
+          <p style={{fontSize: '15px'}}> (Also probably post founders day)</p>
+
+
         </div>
       );
     } else {
@@ -25,6 +33,8 @@ class App extends React.Component {
         <div className = "App-notThrowing">
           <p style={{fontSize: '80px', margin: 0}}> No. </p>
           <p style={{fontSize: '15px'}}> {this.state.date} </p>
+          <p style={{fontSize: '15px'}}> (Also probably post founders day)</p>
+
         </div>
       );
     }
