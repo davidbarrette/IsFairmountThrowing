a48a001051fa9942c99600fb4877aee6de99bc46
dbarrett <dbarrett@macalester.edu>
2020-07-04 01:14:48 -0500

    Added routing capabilities, cleaned up the main js file, more to come

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import './App.css';
-import frog1 from './assets/frog1.png'
-import frog2 from './assets/frog2.png'
-import frog3 from './assets/frog3.png'
-import plague1 from './assets/plague1.png'
-import plague2 from './assets/plague2.png'
 
+import Throwing from './Components/ThrowingComponent/Throwing.js'
+import NavugationBar from './Components/NavigationBar/NavigationBar.js'
+
+import HomePage from './Components/Pages/HomePage.js'
+import PicturePage from './Components/Pages/PicturePage.js'
+
+import {BrowserRouter, Route, Switch } from 'react-router-dom';
+import NavigationBar from './Components/NavigationBar/NavigationBar.js';
 
 
 
@@ -14,37 +16,28 @@ class App extends React.Component {
     super(props);
     this.state = {
       throwing: true,
-      date: "Darty September 7th 2020"
+      partydate: "EXAMPLE DATE MM/DD/YYYY"
     }
   }
 
 
-
   renderScreen() {
-    if(this.state.throwing) {
       return (
-        <div className = "App-throwing">
-          <p style={{fontSize: '100px', margin: 0}}> YES </p>
-          <p style={{fontSize: '30px'}}> {this.state.date} </p>
+        <div className = "App">
+          <Throwing
+            throwing = {this.state.throwing} 
+            partydate={this.state.partydate}/>
 
-          <div style ={{flex: 1, alignItems: 'horizontal', margin: 'auto'}}>
-
-          </div>
-
-
-
+          <BrowserRouter>
+          <NavigationBar/>
+            <Switch>
+              <Route path='/' component ={HomePage} exact/>
+              <Route path='/PicturePage' component ={PicturePage} exact/>
+            </Switch>
+          </BrowserRouter>
 
         </div>
       );
-    } else {
-      return (
-        <div className = "App-notThrowing">
-          <p style={{fontSize: '80px', margin: 0}}> No. </p>
-          <p style={{fontSize: '15px'}}> {this.state.date} </p>
-
-        </div>
-      );
-    }
   }
 
   render() {
---
+++ src/Components/ThrowingComponent/Throwing.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Throwing.css';
+
+export default class IsThrowing extends React.Component{
+
+
+    render(){
+      if(this.props.throwing){
+        return (
+          <div className = "header-container">
+            <div className = "App-throwing">
+                <p style={{fontSize: '100px', margin: 0}}> Yes! </p>
+                <p style={{fontSize: '30px'}}> {this.props.partydate} </p>
+            </div>
+          </div>
+          );
+        }
+      else {
+        return (
+          <div className = "header-container">
+            <div className = "App-notThrowing">
+              <p style={{fontSize: '80px', margin: 0}}> No. </p>
+              <p style={{fontSize: '15px'}}> Check Again Soon </p>
+    
+              <div style ={{flex: 1, alignItems: 'horizontal', margin: 'auto'}}>
+    
+              </div>
+            </div>
+          </div>
+        );
+      }
+    }
+}
